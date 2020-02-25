0d2492dff14130b8a5f5225734afccd08cc7a8f8
brkvolk <49192761+brkvolk@users.noreply.github.com>
2020-02-25 20:09:25 +0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -3,14 +3,16 @@ import random
 import math
 
 
-T = 0.1 #time period
-la = 20.0 #frequencie 
-ny = 0.005 #backprop learning parametr
+T = 0.1 #временной период
+la = 20.0 #частота
+ny = 0.005 #скорость обучения
+I=2#нейроны на входе
+H=2#нероны на скрытом слое
 
-Wih = np.random.uniform(0, 0.2, (2, 2)) #matrixes of waights of input->hiden, hiden->output layers
-Who = np.random.uniform(0, 0.2, 2)
+Wih = np.random.uniform(0, 0.2, (I, H)) #матрицы весов
+Who = np.random.uniform(0, 0.2, H)
 
-#rules of waight`s change
+#правила для весов
 def dwoh( spike_d, spike_a, spike_h):
     return -ny*(spikeIP(spike_a, spike_h)-spikeIP(spike_d, spike_h))
 
@@ -20,7 +22,7 @@ def dwhi( spike_d, spike_a,spike_i,who):
 def Error( spike_d, spike_a):
     return 0.5*(spikeIP(spike_a, spike_a)-2*spikeIP(spike_a, spike_d)+spikeIP(spike_d, spike_d))
 
-def gen_spike(): #generate spikes using Poisson process
+def gen_spike(): #генерируем спайки пуассоновским процессом
     spike = []
     t = 0
     k = 0
@@ -31,11 +33,11 @@ def gen_spike(): #generate spikes using Poisson process
         spike.append(t)
     return spike, k
 
-def timeIP(tm , tn):#inner product of spike times
+def timeIP(tm , tn):#скалярное произведение моментов времени
     sigma=2.0
-    return math.exp(-(abs(tm-tn)**2)/(2*sigma))
+    return math.exp( - (abs(tm - tn) ** 2) / (2 * sigma))
 
-def spikeIP(spike1, spike2):#сinner product of spike series
+def spikeIP(spike1, spike2):#скалярное произведение спайков
    S=0
    for tm in spike1:
        for tn in spike2:
@@ -50,57 +52,58 @@ def spike_sum (spike1, spike2):
      return spike_merge
 
 def multipl(spike, k):
-    spike = [i * k for i in spike if i*k <= 0.1]
+    spike = [i * k for i in spike if i * k <= 0.1]
     return spike
 
 def sigmoid(x):
-  return (1 / (1 + math.exp(-x)))
+  return (1 / (1 + math.exp(- x)))
 
-#SRM neuron model
-tau = 0.005#constant of postsynaptic potential
-tauR = 0.05#refractory period
-nu = 1 #neuron threshold
-tR = 0.001 # absolut refractory period
+#SRM
+tau = 0.005
+tauR = 0.05
+nu = 1 #порог
+tR = 0.001 # макс рассстояние между 2 спайками
 def eta_refract(t):
     if t > 0.:
-        return -nu*math.exp(-t/tauR)
+        return - nu * math.exp(- t / tauR)
     else:
         return 0
 def spike_response(t):
     if t > 0.:
-        return t*math.exp(1-(t/tau))/tau
+        return t*math.exp(1 - (t / tau)) / tau
     else:
         return 0
 
-def closest_t(t, spike):#nearest to the left of t single spike
-    с = 1
-    for i in spike :
-        if t - i > 0 :
+def closest_t(t, spike):#ближайший слева от т одиночный спайк
+    с =  1
+    for i in spike:
+        if t - i > 0:
             c = i
-        else :
+        else:
             break
     return c
 
-def neuron_state(t, spikes, vect_of_w):#how to consider tR????
-    S = 0
-    k = 0
-    tf = 1
-    for spike in spikes :
+def neuron_state(t, spikes, vect_of_w):# осталось учесть tR
+    S = 0#сумма вкладов спайков в потенциал
+    k = 0# номер синапса
+    tf = 1# последний спайк
+    for spike in spikes:
         spike_sum = 0
         tf_loc = closest_t(t, spike)
-        for i in spike:
-             spike_sum += spike_response(t - i)
-        S = S + spike_sum * vect_of_w[k]
+        if t-tf_loc < tR: #условие на абс релакс
+            for i in spike:
+                spike_sum += spike_response(t - i)
+            S = S + spike_sum * vect_of_w[k]
         k += 1
-        if tf == 1 :
-            tf = tf_loc
-        elif (tf < tf_loc) & (tf_loc < t) :
+        if tf == 1:
+            tf=tf_loc
+        elif (tf < tf_loc)&(tf_loc < t):
             tf = tf_loc
 
     return eta_refract(t - tf) + S
 
 
-#just check if i am screwed up
+
 
 max_epoch=500
 
